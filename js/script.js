e74554e0b8b4081d3dc23f06e782f9410e7523e5
Cianuro-83 <buzzeosimone@yahoo.com>
2023-03-22 16:23:01 +0100

    IMPOSTAZIONE ESERCIZIO

---
+++ js/script.js
@@ -0,0 +1,22 @@
+const { createApp } = Vue;
+
+createApp({
+  data() {
+        return {
+            titolo: "to do list in php e json"
+
+ //********************
+        }
+      //FINE RETURN & DATA
+    };
+
+  //******************** */
+  methods: {
+   
+    
+    //********************
+  },
+  // FINE METHODS
+  //******************** */
+
+}).mount("#app");
---
+++ server.php
@@ -0,0 +1,10 @@
+<?php
+// leggo il contenuto del file json
+$todoList_string= file_get_contents("./todoList.json");
+
+
+
+
+
+
+?>
